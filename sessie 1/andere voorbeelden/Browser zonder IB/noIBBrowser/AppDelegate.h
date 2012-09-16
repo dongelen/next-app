a28ac826364fd611c6b4e979ec6566dcff3e595e
Thom Hoekstra <thomhoekstra@live.nl>
2012-09-16 19:07:10 +0200

    Voegt een voorbeeld webbrowser toe waarin geen gebruik is gemaakt van IB

---
+++ sessie 1/andere voorbeelden/Browser zonder IB/noIBBrowser/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  noIBBrowser
+//
+//  Created by Thom Hoekstra on 16-09-12.
+//  Copyright (c) 2012 Thom Hoekstra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
