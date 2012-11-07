329427cb15fe6fb76472d60c3e0d0a3d47c6051b
Raymond van Dongelen <dongelen@nhl.nl>
2012-11-07 14:37:10 +0100

    Sessie 9, met juiste voorbeelden

---
+++ sessie 8/voorbeelden/HaalIP/HaalIP/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HaalIP
+//
+//  Created by Raymond Van Dongelen on 11/7/12.
+//  Copyright (c) 2012 Raymond Van Dongelen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
